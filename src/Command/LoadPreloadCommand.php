5c4db612317314a78ae87ba75a71543498f9de94
Michee NKUSU <nkusumiche1367@gmail.com>
2024-12-20 21:14:54 +0100

    add command download preload

---
+++ src/Command/LoadPreloadCommand.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Command;
+
+use App\Entity\ProductorPreload;
+use App\Repository\ProductorPreloadRepository;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+#[AsCommand(
+    name: 'app:load-preload',
+    description: 'Add a short description for your command',
+)]
+class LoadPreloadCommand extends Command
+{
+    public function __construct(
+        private ProductorPreloadRepository $repository,
+        private ContainerInterface $container
+    ) 
+    {
+        parent::__construct();
+    }
+    protected function configure(): void
+    {
+        $this
+        //->addArgument('arg1', InputArgument::OPTIONAL, 'Argument description')
+            //->addOption('option1', null, InputOption::VALUE_NONE, 'Option description')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $io = new SymfonyStyle($input, $output);
+        $data_arr =[ '"ID";"Nom";"PostNom"; "PréNom";"Structure";"Secteur";"Ville";"Commune Activité";"Quartier Activité";"Adresse Activité"'];
+        $projectDir = $this->container->getParameter('kernel.project_dir');
+        
+        $count_shunk = 1000;
+        $i = 0;
+
+        do {
+            $offset = ($i*1000)+1;
+            
+            $data = $this->repository->findBy([], ["id"=>"Desc"], $count_shunk, $offset);
+            foreach ($data as $key => $item) 
+            {
+                $itemArray = [
+                    '\"'.$item->getId().'\"',
+                    '\"'.$item->getName().'\"',
+                    '\"'.$item->getLastname().'\"',
+                    '\"'.$item->getFileName().'\"',
+                    '\"'.$item->getStructure().'\"',
+                    '\"'.$item->getSector().'\"',
+                    '\"'.$item->getCityEntity()?->getName().'\"',
+                    '\"'.$item->getQuarter().'\"',
+                    '\"'.$item->getAddress().'\"',
+                ];
+                array_push($data_arr, $itemArray);
+            }
+            
+            $i++;
+
+        } while (count($data));
+
+        $data_str = implode("\n", $data_arr);
+        
+        file_put_contents($projectDir."/download_preload_all.csv", $data_str);
+
+
+
+        $io->success('You have a new command! Now make it your own! Pass --help to see your options.');
+
+        return Command::SUCCESS;
+    }
+}
